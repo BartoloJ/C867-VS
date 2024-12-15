7951e724d012a5a2502eb54eaeeb443d628dd29d
Jonathan Bartolo <jonathan.e.bartolo@gmail.com>
2024-12-15 00:10:41 -0800

    default values

--- C867 VS/student.cpp
+++ C867 VS/student.cpp
@@ -4,4 +4,15 @@
 //
 //  Created by Jon on 12/14/24.
 //
-
+#include "student.h"
+Student::Student()
+{
+	this->studentID = "";
+	this->firstName = "";
+	this->lastName = "";
+	this->emailAddress = "";
+	this->age = 0;
+	for (int i = 0; i < daysArraySize; i++) this->daysToComplete[i] = 0;
+	this->degreeProgram = DegreeProgram::SECURITY;
+	
+}
