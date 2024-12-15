df370ffd5e1f8980a6f6d1040bd50a872da2ec51
Jonathan Bartolo <jonathan.e.bartolo@gmail.com>
2024-12-15 14:38:43 -0800

    methods, print header, print

--- C867 VS/student.cpp
+++ C867 VS/student.cpp
@@ -30,3 +30,40 @@ Student::Student(string studentID, string firstName, string lastName, string ema
 }
 
 Student::~Student() {}
+
+string Student::getStudentID() { return this->studentID; }
+string Student::getFirstName() { return this->firstName; }
+string Student::getLastName() { return this->lastName; }
+string Student::getEmailAddress() { return this->emailAddress; }
+int Student::getAge() { return this->age; }
+int Student::getDaysToComplete() { return this->daysToComplete; }
+DegreeProgram Student::getDegreeProgram() { return this->degreeProgram; }
+
+void Student::setStudentID(string studentID) { this->studentID = studentID; }
+void Student::setFirstName(string firstName) { this->firstName = firstName; }
+void Student::setLastName(string lastName) { this->lastName = lastName; }
+void Student::setEmailAddress(string emailAddress) { this->emailAddress = emailAddress; }
+void Student::setAge(int age) { this->age = age; }
+void Student::setDaysToComplete(int daysToComplete[]) 
+{
+	for (int i = 0; i < daysArraySize; i++) this->daysToComplete[i] = daysToComplete[i];
+}
+void Student::setDegreeProgram(DegreeProgram degreeProgram) { this->degreeProgram = degreeProgram; }
+
+void Student::printHeader()
+{
+	cout << "Output format: StudentID|First Name|Last Name|Email|Age|Days|Degree\n"
+}
+
+void Student::print()
+{
+	cout << this->getStudentID() << '\t';
+	cout << this->getFirstName() << '\t';
+	cout << this->getLastName() << '\t';
+	cout << this->getEmailAddress() << '\t';
+	cout << this->getAge() << '\t';
+	cout << this->getDaysToComplete()[0] << ',';
+	cout << this->getDaysToComplete()[1] << ',';
+	cout << this->getDaysToComplete()[2] << '\t';
+	cout << degreeProgramTypeStrings[this->getDegreeProgram()] << '\n';
+}
